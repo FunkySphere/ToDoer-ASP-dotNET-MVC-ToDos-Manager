1a7c5c57e171abac1190741152c5a2eeb25ed45b
FunkySphere <krauzowiczs42@gmail.com>
2022-11-07 11:48:17 +0100

    ASP.NET mvc template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToDoer_ASP_dotNET_MVC_ToDos_Manager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
