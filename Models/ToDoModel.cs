3bb638ae5d8bcd806272d0b5de7fd843fce837a2
FunkySphere <krauzowiczs42@gmail.com>
2022-11-08 11:53:39 +0100

    todo comments and such

--- Models/ToDoModel.cs
+++ Models/ToDoModel.cs
@@ -13,4 +13,5 @@ public class ToDo
     public bool Complete { get; set; } = false;
 
     //add creation date and deadline date later
+    //also add tags to be able to view these tasks in an organizaed manner
 }
