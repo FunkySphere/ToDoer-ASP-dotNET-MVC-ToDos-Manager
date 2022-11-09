9d9e40931eb621e052b47eb42291185f9b4464b5
FunkySphere <krauzowiczs42@gmail.com>
2022-11-09 14:41:06 +0100

    task table

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -1,17 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoer.Models;
-
+using ToDoer.Data;
 namespace ToDoer.Controllers;
 
 public class ToDoController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public ToDoController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     public IActionResult List()
     {
-        return View();
-    }
-
-    public IActionResult Backlog()
-    {
-        return View();
+        List<ToDo> objList = _db.ToDos.ToList();
+        return View(objList);
     }
 }
