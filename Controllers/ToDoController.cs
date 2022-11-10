90318cd239332cec7ccad8020aa0f5626d98b88b
FunkySphere <krauzowiczs42@gmail.com>
2022-11-10 11:38:23 +0100

    adding items to database functionality

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -16,4 +16,26 @@ public class ToDoController : Controller
         List<ToDo> objList = _db.ToDos.ToList();
         return View(objList);
     }
+
+    //GET Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    //POST Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(ToDo obj)
+    {
+        //validation
+        if(ModelState.IsValid && _db.ToDos != null)
+        {
+            _db.ToDos.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+        return View(obj);
+    }
 }
