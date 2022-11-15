82a82b4c2a58e058566ce7f3e50ece59735443b8
FunkySphere <krauzowiczs42@gmail.com>
2022-11-15 09:08:47 +0100

    updated dbs todo table

--- Models/ToDoModel.cs
+++ Models/ToDoModel.cs
@@ -12,6 +12,14 @@ public class ToDo
 
     public bool Complete { get; set; } = false;
 
-    //add creation date and deadline date later
+    //add creation date and deadline date later// i got u fam
     //also add tags to be able to view these tasks in an organizaed manner
+
+    public DateTime? CreationDate {get; set;} = DateTime.Now;
+
+    //if the deadline is given, add a time left table section
+    public DateTime? Deadline {get; set;}
+
+    //tags separated by coma
+    public string? Tags {get; set;} = "";
 }
